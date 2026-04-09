2c085d3ee073e8bc59833d5c1fa1b21a2d0ecb43
Temirov Avazbek <170247284+tmavazbek@users.noreply.github.com>
2026-04-10 00:14:55 +0500

    yuklandi

--- markaz.py
+++ markaz.py
@@ -1,2 +1,53 @@
 import json
 
+f = open("markaz.json")
+
+dct = {}
+
+data = json.load(f)
+#                    1
+# # for branches in data['branches']:
+#     print(branches['name'])
+
+#                    2
+# # for branches in data['branches']:
+#     for teachers in branches['teachers']:
+#         if teachers['subject'] == 'Python':
+#             print(f"Ism: {teachers['name']}, Branch: {branches['name']}, Tajriba: {teachers['experience']} yil")
+
+#                    3
+# for branches in data['branches']:
+#     count = 0
+#     for students in branches['students']:
+#         count+=1
+#     dct[branches['name']] = count
+# print(dct)
+
+
+#                   4
+# for branch in data['branches']:
+#     all = [student for student in branch['students']]
+#     natija = max(all, key=lambda x: x['payment'])
+# print(f'Eng kop payment: {natija['name']}, Branchi: {branch['name']}')
+
+
+#                   5
+# for branch in data['branches']:
+#     natija = sum(student['payment'] for student in branch['students'])
+#     print(f'{branch['name']}: {natija}')
+
+
+#                    6
+# for branch in data['branches']:
+#     for teacher in branch["teachers"]:
+#         if teacher['experience'] > 5:
+#             print(f"Ismi: {teacher['name']}, Tajribasi: {teacher['experience']} yil")
+
+
+#                    7
+# for branch in data['branches']:
+#     for students in branch['students']:
+#         if students['course'] == 'Python':
+#             print(branch['name'])
+
+f.close()
